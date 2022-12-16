b5657e94f1d448100bbf021d7d004f5cbb0f7b21
@hexlet/cli <support@hexlet.io>
2022-12-16 22:52:05 +0300

    submit java-spring-ru/migrations

--- java-spring-ru/migrations/src/main/java/exercise/controller/PeopleController.java
+++ java-spring-ru/migrations/src/main/java/exercise/controller/PeopleController.java
@@ -7,9 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PathVariable;
 
-import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -25,14 +23,14 @@ public class PeopleController {
     }
 
     // BEGIN
-    @GetMapping (path = "/people")
-        public void showAllPersons (@RequestBody Map <String, Object> persons) {
-            String query = "SELECT * FROM person";
-            jdbc.update(query, persons.get("first_name"), persons.get("last_name"));}
+   @GetMapping (path = "/people")
+       public void showAllPersons(@RequestBody Map<String, Object> persons) {
+       String query = "SELECT * FROM person";
+       jdbc.update(query, persons.get("first_name"), persons.get("last_name")); }
 
-        @GetMapping("/people/{id}")
-            public void getPerson (@RequestBody Map <String, Object> person) {
-            String query = "SELECT * FROM person WHERE id = (?)"; // check
-            jdbc.update(query, person.get("first_name"), person.get("last_name"));}
+   @GetMapping("/people/{id}")
+       public void getPerson(@RequestBody Map<String, Object> person) {
+       String query = "SELECT * FROM person WHERE id = (?)"; // check
+       jdbc.update(query, person.get("first_name"), person.get("last_name")); }
     // END
 }
