e334c83b80b3ca5051e60652a3991179381f0d81
@hexlet/cli <support@hexlet.io>
2024-05-23 13:11:45 +0300

    submit java-spring-ru/response-entity

---
+++ java-spring-ru/response-entity/src/main/java/exercise/Application.java
@@ -0,0 +1,88 @@
+package exercise;
+
+import java.net.URI;
+import java.util.List;
+
+import java.util.Optional;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+
+import exercise.model.Post;
+
+@SpringBootApplication
+@RestController
+public class Application {
+    // Хранилище добавленных постов
+    private List<Post> posts = Data.getPosts();
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    //Список всех постов
+    @GetMapping("/posts")
+    public ResponseEntity<List<Post>> list(@RequestParam(defaultValue = "10") Integer limit) {
+        var result = posts.stream().limit(limit).toList();
+
+        return ResponseEntity.ok()
+                .header("X-Total-Count", String.valueOf(posts.size()))
+                .body(result);
+    }
+
+    //Просмотр конкретного поста
+    @GetMapping("/posts/{id}")
+    public ResponseEntity<Optional<Post>> showPost(@PathVariable String id) {
+        var post = posts.stream()
+                .filter(p -> p.getId().equals(id))
+                .findFirst();
+        if (post.isPresent()) {
+            return ResponseEntity.status(HttpStatus.OK).body(post);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body((post));
+        }
+    }
+
+    //Создание нового поста
+    @PostMapping("/posts")
+    public ResponseEntity<Post> createPost(@RequestBody Post post) {
+        posts.add(post);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(post);
+    }
+
+    //Обновление поста
+    @PutMapping("/posts/{id}")
+    public ResponseEntity<Post> updatePost(@PathVariable String id, @RequestBody Post data) {
+        var postToUpdate = posts.stream()
+                .filter(p -> p.getId().equals(id))
+                .findFirst();
+        if (postToUpdate.isPresent()) {
+            var post = postToUpdate.get();
+            post.setId(data.getId());
+            post.setBody(data.getBody());
+            post.setTitle(data.getTitle());
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(data);
+        } else {
+            return ResponseEntity.status(HttpStatus.NO_CONTENT)
+                    .body(data);
+        }
+    }
+
+    //Удаление поста
+    @DeleteMapping("/posts/{id}")
+    public void destroy(@PathVariable String id) {
+        posts.removeIf(p -> p.getId().equals(id));
+    }
+}
