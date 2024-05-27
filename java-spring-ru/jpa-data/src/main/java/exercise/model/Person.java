224fd61590b1ae0a729cdc951d8c741db964f9d3
ruaabrx <elizaveta.molchanyuk@raiffeisen.ru>
2024-05-27 22:42:23 +0300

    commit

--- java-spring-ru/jpa-data/src/main/java/exercise/model/Person.java
+++ java-spring-ru/jpa-data/src/main/java/exercise/model/Person.java
@@ -5,7 +5,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Table;
 import static jakarta.persistence.GenerationType.IDENTITY;
-
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
